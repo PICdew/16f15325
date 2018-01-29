02007fc1e50a48e241d89c2490276393a82c7cbe
erwin <m9207216@mail.ntust.edu.tw>
2018-01-29 10:04:44 +0800

    verify stable 2s to change led

--- src/ex1.X/main.c
+++ src/ex1.X/main.c
@@ -60,10 +60,27 @@ void set_led(uint8_t led){
         LEDDuty_SetHigh();  //0        
     }
 }
+
+//25ms
+void wait_25ms(void){
+    uint16_t i=0;
+    while(i<16000){
+        i++;
+    }
+}
+void check_time(void){
+    uint16_t i=0;
+    uint16_t j=0;
+    while(i<10000){
+        i++;
+        j++;
+    }
+}
 void main(void)
 {
     uint8_t led=0;
     uint8_t touch_sensor=0;
+    uint16_t cnt_25ms=0;
     // initialize the device
     SYSTEM_Initialize();
 
@@ -81,16 +98,52 @@ void main(void)
 
     // Disable the Peripheral Interrupts
     //INTERRUPT_PeripheralInterruptDisable();
-    printf("uart1 test1\n\r");
-    printf("uart1 test2\n\r");
+    //printf("uart1 test1\n\r");
+    //printf("uart1 test2\n\r");
+    //while(1){
+    //    wait_25ms();
+    //    LEDDuty_Toggle();
+    //}
     //default led status
     led=0;
-    set_led(led);
+    set_led(led); 
     
     while (1)
     {
+        while(PORTCbits.RC1 == 1); //wait change;
+        wait_25ms();
+        
+        //check 1.6s;
+        cnt_25ms = 0;
+        while( (PORTCbits.RC1 == 0) && (cnt_25ms < 30)  ){
+            wait_25ms();
+            cnt_25ms++;
+        }
+        
+        //entry
+        if( cnt_25ms >= 30 ){
+            //low
+            if(led==0){
+                led = 1;
+            }else{
+                led = 0;
+            }
+            set_led(led);            
+        }
+        while(PORTCbits.RC1 == 0); //wait change;
+        
+        //release 0.25s
+        //cnt_25ms = 0;
+        //while(cnt_25ms < 10){
+        //    wait_25ms();
+        //    if( PORTCbits.RC1 == 1) cnt_25ms++;
+        //}
+    }    
+#if 0    
+    while (1)
+    {
         // Add your application code
-        while(PORTCbits.RC1 == 1); //wait chang;
+        while(PORTCbits.RC1 == 1); //wait change;
         
         //low
         if(led==0){
@@ -99,8 +152,9 @@ void main(void)
             led = 0;
         }
         set_led(led);
-        while(PORTCbits.RC1 == 0); //wait chang;
+        while(PORTCbits.RC1 == 0); //wait change;
     }
+#endif
 }
 /**
  End of File
--- src/ex1.X/mcc_generated_files/mcc.c
+++ src/ex1.X/mcc_generated_files/mcc.c
@@ -88,6 +88,7 @@ void SYSTEM_Initialize(void)
 
     PIN_MANAGER_Initialize();
     OSCILLATOR_Initialize();
+    TMR0_Initialize();
     EUSART1_Initialize();
 }
 
